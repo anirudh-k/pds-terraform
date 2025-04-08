eabe1cdc5e1c1e1b524db03a9879d6dd87d3c652
Anirudh Kaushik <anirudh-k@users.noreply.github.com>
2025-04-07 18:28:40 -0700

    Initial terraform, cloud composer (#1)

---
+++ main.tf
@@ -0,0 +1,78 @@
+# ~~~~~~~~~~ Cloud Composer ~~~~~~~~~~
+
+resource "google_project_service" "composer_api" {
+  provider = google
+  project  = var.project
+  service  = "composer.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_service_account" "composer_env_sa" {
+  provider     = google
+  account_id   = "composer-env-service-account"
+  display_name = "Composer Environment Service Account"
+}
+
+resource "google_project_iam_member" "composer_env_sa" {
+  provider = google
+  project  = var.project
+  member   = format("serviceAccount:%s", google_service_account.composer_env_sa.email)
+  role     = "roles/composer.worker"
+}
+
+resource "google_composer_environment" "airflow_environment" {
+  provider = google
+  name     = "airflow"
+
+  config {
+
+    software_config {
+      image_version = "composer-3-airflow-2.10.2-build.12"
+    }
+
+    database_config {
+      zone = var.zone
+    }
+
+    workloads_config {
+
+      scheduler {
+        cpu        = 1
+        memory_gb  = 2.5
+        storage_gb = 2
+        count      = 1
+      }
+      triggerer {
+        count     = 1
+        cpu       = 0.5
+        memory_gb = 1
+      }
+      web_server {
+        cpu        = 1
+        memory_gb  = 2.5
+        storage_gb = 2
+      }
+      worker {
+        cpu        = 1
+        memory_gb  = 2
+        storage_gb = 2
+        min_count  = 2
+        max_count  = 4
+      }
+    }
+
+    environment_size = "ENVIRONMENT_SIZE_SMALL"
+
+    node_config {
+      service_account = google_service_account.composer_env_sa.email
+    }
+
+  }
+
+  labels = {
+    owner   = "data-platform"
+    env     = "dev"
+    service = "airflow"
+  }
+}
